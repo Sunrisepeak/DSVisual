2d1636fe4c63c3143e67781a1814414bba68acf5
SPeak <speakshen@163.com>
2023-09-14 01:29:00 +0800

    bugfix/optimize: fix issue #2 and optimize

---
+++ animate/HanimExtends.hpp
@@ -0,0 +1,9 @@
+
+
+#ifndef __HANIM_EXTENDS_HPP__DSVISUAL
+#define __HANIM_EXTENDS_HPP__DSVISUAL
+
+#include <animate/HanimAnimate.dsvisual.hpp>
+#include <animate/HanimObject.dsvisual.hpp>
+
+#endif
